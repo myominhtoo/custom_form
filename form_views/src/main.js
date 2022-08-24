f47beaecd8974a1bb715db2c09aca53f8b971df7
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-08-24 21:51:40 +0630

    added tables.txt

--- form_views/src/main.js
+++ form_views/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/style/common.css';
 import App from './App.vue'
 import store from './store/index';
 import router from './router/index';
+
 
 createApp(App).use(store).use(router).mount('#app')
--- form_views/src/pages/Hello.vue
+++ form_views/src/pages/Hello.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1>Hello I' root pages</h1>
+    <h1 class="">Hello I' root pages</h1>
 </template>
