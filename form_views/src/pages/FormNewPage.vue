6197ff494e8fee1074a2d2b83a1416928ffa5e4e
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-08-31 00:29:56 +0630

    added view logic for form creating in FormNewPage.vue

--- form_views/src/pages/FormNewPage.vue
+++ form_views/src/pages/FormNewPage.vue
@@ -1,18 +1,55 @@
 <template>
-   <main class="w-auto h-auto">
+   <main class="w-auto">
         <Navbar/>
         
         <div id="main" class="w-75 mx-auto my-5" style="height:300px;">
-            <header id="form-header" class="bg-snow py-5 px-5">\
+            <header id="form-header" class="py-5 px-5">
                 <h2 class="h2 violet">Form Title</h2>
-                <input type="text" v-model="formData.title" class="form-control mx-auto">
+                <input type="text"
+                  v-model="formData.title"
+                  class="w-100 mx-auto" 
+                  placeholder="Enter form's title "/>
             </header>
 
-            <main id="form-main">
-                <div id="part" class="py-5 px-5">
-                    
+            <main id="form-main" class="my-4">
+                <div v-for="part,index in formData.parts" :key="part.id" id="part" class="py-5 px-5">
+                    <h4 class="h5 violet my-2">({{ index + 1 }})Question</h4>
+                    <div class="w-100 mx-auto d-flex gap-3">
+                        <input
+                         type="text"
+                         v-model="part.title" 
+                         class="w-100 mx-auto"
+                         placeholder="Enter question "/>
+                         
+                         <select v-model="part.type" class="form-select w-25 text-capitalize">
+                            <option disabled selected >Choose type for question</option>
+                            <option v-for="partType in partTypes" :key="partType.id" :value="partType.id">{{ partType.name }}</option>
+                         </select>
+                    </div>
+                    <br/>
+                    <div id="part-answers" class="d-flex flex-column gap-3">
+                        <div 
+                          v-for="answer,idx in part.answers"
+                          :key="idx"
+                          class="w-auto">
+                            <input 
+                             v-model="answer.value"
+                             type="text"
+                             id="part-answer"
+                             :placeholder="'Answer '+(idx+1)" />
+                            <!-- index is from parent , idx is child -->
+                            <span 
+                              @click="handleDeleteAnswer( index , idx )"
+                              id="answer-del-btn"><i class="fa-solid fa-trash-can"></i></span>
+                        </div>
+                    </div>
+                    <a 
+                     v-if="part.answers.length < 4 "
+                     @click="handleAddOption( index )" class="h6 d-block my-3" >Add Option</a>
                 </div>
             </main>
+            <br/>
+            <br/>
         </div>
    </main>
 </template>
@@ -20,23 +57,74 @@
 <script setup >
 import Navbar from '../components/Navbar.vue';
 import Control from '../components/creation/Control.vue';
-import { onMounted, reactive } from "@vue/runtime-core";
+import { onMounted, reactive , ref } from "@vue/runtime-core";
+import axios from 'axios';
 
 onMounted(() => {
     document.title = "New | Forms";
 });
+
+const partTypes = ref([
+    {
+        id : 1,
+        name : "text"
+    },
+    {
+        id : 2,
+        name : "radio"
+    },
+    {
+        id : 3,
+        name : "checkbox"
+    }
+]);
 
 const formData = reactive({
     title : "Untitled title",
     parts : [
         {
             id : 1,
-            type : "text",
+            type : 1,
             title : "",
-            answers : [],
+            answers : [ {
+                id : 1,
+                value : "",
+            } ],
+            key : "",
+        },
+        {
+            id : 2,
+            type : 1,
+            title : "",
+            answers : [ {
+                // id : 1,
+                value : "",
+            } ],
             key : "",
         }
     ]
+});
+
+const handleAddOption = ( target ) => { 
+
+    let prevAnswers = formData.parts[target].answers;
+
+    if( prevAnswers.length < 4 ){
+        formData.parts[target].answers.push({
+            // id : prevAnswers.length + 1,
+            value : "",
+        });
+    }
+} 
+
+const handleDeleteAnswer = ( questionIdx , answerIdx ) => {
+
+    formData.parts[questionIdx].answers.splice( answerIdx , 1 );
+
+}
+
+onMounted(() => {
+    
 });
 
 
